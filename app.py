493286333dfbe1e9f18ee28f11ec4aaea774e278
EliasJavierRiquelme <elias.renault4@gmail.com>
2024-05-11 20:40:12 +0200

    Add delete and search in html, Add CCS, img and function DELETE

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request, render_template, url_for ##Render template nos ayuda a publicar o enlazar el html
+from flask import Flask, jsonify, request, render_template, url_for
+from sqlalchemy import or_ ##Render template nos ayuda a publicar o enlazar el html
 
 ##Ahora vamos a importar el formato de tabla que hemos creado en Models.py
 from Models import db, books
@@ -37,6 +38,14 @@ db.init_app(app)  ##Esto es para que sepa donde debe iniciar la base de datos ""
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/search.html", methods=["GET"])
+def search():
+    return render_template("search.html")
+
+@app.route("/delete.html", methods=["GET"])
+def delete():
+    return render_template("delete.html")
 
 
 @app.route("/api/v1/books", methods=["GET"])
@@ -81,7 +90,7 @@ def getBook():
         if "score" in request.args:
             fields["score"] = request.args["score"]
 
-        book = books.query.filter_by(**fields)[1]
+        book = books.query.filter_by(**fields).first()
         if not book:
             return jsonify({"message": "No se ha encontrado el libro"}), 404
         else:
@@ -109,6 +118,48 @@ def addBook():
     except Exception:
         exception("\n [SERVER]: Error en la ruta /api/addbook. log: \n")
         return jsonify({"message": "Error al cargar el libro!!"}), 500
+
+
+## Aca voy a crear la funcion para buscar libros a traves del fomulario creado en templates
+
+
+@app.route("/api/search", methods=["POST"])
+def searchBook():
+    try:
+        titleBook = request.form["title"]
+        book = books.query.filter(books.title.like(f"%{titleBook}%")).first()
+        if not book:
+            return jsonify({"message": "No se ha encontrado el libro"}), 404
+        else:
+            return jsonify(book.serialize()), 200
+    except Exception:
+        exception("Falla el servidor!! ")
+        return jsonify({"message": "Falla el servidor!!"}), 500
+
+@app.route("/api/delete", methods=["DELETE"])
+def deleteBook():
+    try:
+        # Obtener el título del libro de la solicitud DELETE
+        title = request.args.get("title")
+
+        # Buscar el libro por su título en la base de datos
+        book = books.query.filter_by(title=title).first()
+        if not book:
+            # Si el libro no existe, devuelve un mensaje de error
+            return jsonify({"message": "El libro no existe"}), 404
+        
+        # Eliminar el libro de la base de datos
+        db.session.delete(book)
+        db.session.commit()
+
+        # Devolver un mensaje de éxito
+        return jsonify({"message": "Libro eliminado exitosamente"}), 200
+
+    except Exception as e:
+        # Si ocurre algún error durante el proceso, devuelve un mensaje de error interno del servidor
+        exception(f"Falla en el servidor al eliminar el libro: {str(e)}")
+        return jsonify({"message": "Error interno del servidor al eliminar el libro"}), 500
+
 
 
 if __name__ == "__main__":
